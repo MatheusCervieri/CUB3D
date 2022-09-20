054dd7eaf43ea4e665553d38b3ac60f124ac7924
Moacir Amaro <moacirjgt@gmail.com>
2022-09-20 19:25:26 -0300

    Add arguments validation to main function

--- srcs/main.c
+++ srcs/main.c
@@ -3,18 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 20:23:53 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/09/20 09:29:56 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/09/20 19:20:52 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	int		fd;
+	if(!validate_arguments(argc, argv))
+		return (1);
+	
+/* 	int		fd;
 	char	*map_line;
 	data = malloc(sizeof(t_data));
 	data->map_string = ft_strdup("");
@@ -33,5 +36,5 @@ int	main(void)
 
 	
 	data->player_nbs = 0; 
-	parse_map();
+	parse_map(); */
 }
