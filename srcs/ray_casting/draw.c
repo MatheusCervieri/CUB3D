cc5a0e430faa0c1c163834946a1168e3bd918839
MatheusCervieri <Matheus Cervieri>
2022-10-05 11:51:19 -0300

    I fix draw.c norm

--- srcs/ray_casting/draw.c
+++ srcs/ray_casting/draw.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:59:31 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/10/05 12:52:27 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/10/05 11:50:41 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	img_pix_get(t_img *img, int x, int y)
 float	distance_btw_two_points(float x1, float y1, float x2, float y2)
 {
 	float	distance;
-	
-	distance = ((x2 - x1) * (x2-x1)) + ((y2-y1) * (y2-y1));
+
+	distance = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
 	return (sqrt(distance));
 }
 
@@ -37,8 +37,8 @@ void	init_img(t_data *data, t_img *img, int height, int width)
 
 void	init_imgs(t_data *data)
 {
-	init_img(data, &data->game_img, WINDOW_WIDTH , WINDOW_HEIGHT);
-	init_img(data, &data->mm_bg_img, BACKGROUND_SIZE , BACKGROUND_SIZE);
+	init_img(data, &data->game_img, WINDOW_WIDTH, WINDOW_HEIGHT);
+	init_img(data, &data->mm_bg_img, BACKGROUND_SIZE, BACKGROUND_SIZE);
 }
 
 void	draw_3d_world(t_data *data)
@@ -48,24 +48,29 @@ void	draw_3d_world(t_data *data)
 	float	y_texture;
 	float	y_texture_step;
 
-	render_square(&data->game_img, *data->ceiling_color, WINDOW_WIDTH, WINDOW_HEIGHT, 0 , 0);	
-	render_square(&data->game_img, *data->floor_color, WINDOW_WIDTH, WINDOW_HEIGHT/2, 0 , 0);	
+	render_square(&data->game_img, *data->ceiling_color, WINDOW_WIDTH,
+		WINDOW_HEIGHT, 0, 0);
+	render_square(&data->game_img, *data->floor_color, WINDOW_WIDTH,
+		WINDOW_HEIGHT / 2, 0, 0);
 	i = 0;
 	while (i < 320)
 	{
 		y_texture = 0;
-		y_texture_step = 64/(float)data->rays[i].line_height; //32 é o tamanho da textura
+		y_texture_step = 64 / (float) data->rays[i].line_height;
 		j = 0;
 		while (j < data->rays[i].line_height)
 		{
-			data->rays[i].pixel = img_pix_get(&data->texture_img[data->rays[i].position], (int) data->rays[i].x_texture , (int)y_texture);
-			img_pix_put(&data->game_img, 0 + i, j + data->rays[i].line_o, data->rays[i].pixel);
-			y_texture+= y_texture_step; 
+			data->rays[i].pixel = img_pix_get(&data->texture_img[data->rays[i].position],
+					(int) data->rays[i].x_texture, (int) y_texture);
+			img_pix_put(&data->game_img, 0 + i, j + data->rays[i].line_o,
+				data->rays[i].pixel);
+			y_texture+= y_texture_step;
 			j++;
 		}
 	i++;
 	}
-	mlx_put_image_to_window(data->mlx, data->win_ptr, data->game_img.mlx_img , 0, 0);
+	mlx_put_image_to_window(data->mlx, data->win_ptr,
+		data->game_img.mlx_img, 0, 0);
 }
 
 void	draw(t_data *data)
