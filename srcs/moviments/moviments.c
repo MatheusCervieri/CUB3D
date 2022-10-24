b91d877ebd4c6b65df9c0e8e379fe681a09bdc43
MatheusCervieri <mathcervieri@gmail.com>
2022-10-25 00:31:28 +0200

    Tentei fazer uma colisao mas nao deu certo

--- srcs/moviments/moviments.c
+++ srcs/moviments/moviments.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 21:35:32 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/10/05 16:06:12 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/10/25 00:30:13 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,11 @@ void	move_left(t_data *data)
 
 void	move_up(t_data *data)
 {
+	if(check_intersections_c_ray(data) == 0)
+	{
 	data->player.y = data->player.y + sin(data->player.rotation) * 5;
 	data->player.x = data->player.x + cos(data->player.rotation) * 5;
+	}
 }
 
 void	move_down(t_data *data)
