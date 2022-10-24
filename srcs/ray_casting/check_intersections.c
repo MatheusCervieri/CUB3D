b80a53f1290e5a4a97355e756b512d42907e7086
MatheusCervieri <mathcervieri@gmail.com>
2022-10-24 19:43:19 +0200

    I fix one more crazy bug

--- srcs/ray_casting/check_intersections.c
+++ srcs/ray_casting/check_intersections.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 15:39:00 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/10/11 04:44:43 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/10/24 19:40:39 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 int	calculate_horizontal_intersections(t_data *data,
 	float rounded_down_number, float rotation)
 {
+	(void)rounded_down_number;
 	if (rotation > PI)
 	{
-		data->ry = rounded_down_number * (MINI_MAP_SIZE) - 1;
+		data->ry = (((int)data->player.y>>6)<<6) -0.0001;
 		data->rx = (data->player.y - data->ry) * data->tan + data->player.x;
 		data->yo = -MINI_MAP_SIZE;
 		data->xo = -data->yo * data->tan;
@@ -25,7 +26,7 @@ int	calculate_horizontal_intersections(t_data *data,
 	}
 	else if (rotation < PI)
 	{
-		data->ry = rounded_down_number * (MINI_MAP_SIZE) + MINI_MAP_SIZE;
+		data->ry = (((int)data->player.y>>6)<<6)+64;
 		data->rx = (data->player.y - data->ry) * data->tan + data->player.x;
 		data->yo = MINI_MAP_SIZE;
 		data->xo = -data->yo * data->tan;
@@ -50,9 +51,10 @@ void	check_intersections_h(t_data *data, float *new_x,
 int	calculate_vertical_intersections(t_data *data,
 	float rounded_down_number, float rotation)
 {
+	(void)rounded_down_number;
 	if (rotation > PI / 2 && rotation < 3 * PI / 2)
 	{
-		data->rx = rounded_down_number * (MINI_MAP_SIZE) - 1;
+		data->rx = (((int)data->player.x>>6)<<6) -0.0001;
 		data->ry = (data->player.x - data->rx) * data->tan + data->player.y;
 		data->xo = -MINI_MAP_SIZE;
 		data->yo = -data->xo * data->tan;
@@ -60,7 +62,7 @@ int	calculate_vertical_intersections(t_data *data,
 	}
 	if (rotation < PI / 2 || rotation > 3 * PI / 2)
 	{
-		data->rx = rounded_down_number * (MINI_MAP_SIZE) + MINI_MAP_SIZE;
+		data->rx = (((int)data->player.x>>6)<<6)+64; 
 		data->ry = (data->player.x - data->rx) * data->tan + data->player.y;
 		data->xo = MINI_MAP_SIZE;
 		data->yo = -data->xo * data->tan;
