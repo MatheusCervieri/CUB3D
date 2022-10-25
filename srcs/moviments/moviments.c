3bacf108e61a7306d9e07e4ddec4b2c59ec0ccbb
MatheusCervieri <mathcervieri@gmail.com>
2022-10-25 03:51:15 +0200

    I made the first collision

--- srcs/moviments/moviments.c
+++ srcs/moviments/moviments.c
@@ -48,7 +48,7 @@ int	is_wall(t_data *data, float x, float y)
 void	move_up(t_data *data)
 {
 
-	int xo=0; if (data->player.rotation > PI / 2 && data->player.rotation < 3 * PI / 2){ xo=-20;} else{ xo=20;}                                    
+	int xo=0; if (data->player.rotation > PI / 2 && data->player.rotation < 3 * PI / 2){ xo=-110;} else{ xo=110;}                                    
  	int yo=0; if (data->player.rotation > PI){ yo=-110;} else{ yo=110;}                                   
  	int ipx=data->player.x, ipx_add_xo=(data->player.x+xo), ipx_sub_xo=(data->player.x-xo);             
  	int ipy=data->player.y, ipy_add_yo=(data->player.y+yo), ipy_sub_yo=(data->player.y-yo);   
@@ -75,6 +75,20 @@ void	move_up(t_data *data)
 
 void	move_down(t_data *data)
 {
+	int xo=0; if (data->player.rotation > PI / 2 && data->player.rotation < 3 * PI / 2){ xo=-110;} else{ xo=110;}                                    
+ 	int yo=0; if (data->player.rotation > PI){ yo=-100;} else{ yo=100;}                                   
+ 	int ipx=data->player.x,ipx_sub_xo=(data->player.x-xo);             
+ 	int ipy=data->player.y,ipy_sub_yo=(data->player.y-yo);   
+	
+	if(is_wall(data, ipx, ipy_sub_yo) == 0)
+	{
 	data->player.y = data->player.y - sin(data->player.rotation) * 5;
+	}
+	if(is_wall(data, ipx_sub_xo, ipy) == 0)
+	{
 	data->player.x = data->player.x - cos(data->player.rotation) * 5;
+	}
+	
+	
+	
 }
